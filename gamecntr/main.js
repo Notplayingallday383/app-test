36307cf7ec66a07ba09735ef5858ca6e42f4666b
John <playingallday383@gmail.com>
2024-02-04 10:43:36 -0500

    buf fix

--- gamecntr/main.js
+++ gamecntr/main.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
     const welcome = document.getElementById('welcome')
-    const user = window.parent.tb.user.username
+    const user = await window.parent.tb.user.username()
     if (localStorage.getItem('welcome') === 'true') {
         welcome.innerText = `Welcome back ${user}`
     } else {
